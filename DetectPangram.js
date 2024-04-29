f1c1c3b08ba6734824eaf96034e8e56229cb09d1
Mike Vautour <mike@iglab.dev>
2024-04-29 00:03:11 -0400

    Clean up comments

--- DetectPangram.js
+++ DetectPangram.js
@@ -1,4 +1,8 @@
-/* Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation. */
+/*
+  Given a string, detect whether or not it is a pangram. 
+  Return True if it is, False if not. 
+  Ignore numbers and punctuation. 
+*/
 
 function isPangram(string) {
   const strSet = new Set(string.toLowerCase().replace(/[^a-z]/g, ""));
